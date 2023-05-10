c266ef8ff6a390eca183ffcf4e1509a070caf6dd
jofa446 <123947876+jofa446@users.noreply.github.com>
2023-05-10 13:13:33 +0200

    Add files via upload

---
+++ M05JUnitMaven/src/main/java/com/mycompany/m05junitmaven/M05JUnitMaven.java
@@ -0,0 +1,21 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.m05junitmaven;
+
+/**
+ *
+ * @author iliya
+ */
+public class M05JUnitMaven {
+    
+    
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+    
+}
+
+
