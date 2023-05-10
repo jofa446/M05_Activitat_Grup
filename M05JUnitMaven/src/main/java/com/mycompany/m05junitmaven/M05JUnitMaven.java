3d1d4052dcd13677b903d5c62ccf9f97021f112b
Iliyan74 <118401742+Iliyan74@users.noreply.github.com>
2023-05-10 12:48:46 +0200

    Add files via upload

---
+++ M05JUnitMaven/src/main/java/com/mycompany/m05junitmaven/M05JUnitMaven.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.m05junitmaven;
+
+/**
+ *
+ * @author iliya
+ */
+public class M05JUnitMaven {
+    
+
+  public static int add(int a, int b) {
+        return a + b;
+    }
+  
+}
