7fe0536bbf2ec69c9ecda5b4cca41dd8bf66c7f1
jofa446 <123947876+jofa446@users.noreply.github.com>
2023-05-10 12:04:32 +0200

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenprojectm05;
+
+/**
+ *
+ * @author alumne
+ */
+public class MyClass {
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+}
