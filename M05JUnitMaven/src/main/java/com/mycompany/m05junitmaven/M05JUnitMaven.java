f4580c61552114dfc7a653b7aa77f246d7907978
danielbirlan <123947969+danielbirlan@users.noreply.github.com>
2023-05-10 13:03:53 +0200

    Add files via upload

--- M05JUnitMaven/src/main/java/com/mycompany/m05junitmaven/M05JUnitMaven.java
+++ M05JUnitMaven/src/main/java/com/mycompany/m05junitmaven/M05JUnitMaven.java
@@ -1,27 +1,12 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-iliyan
-
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
-main
  */
 
 package com.mycompany.m05junitmaven;
 
 /**
  *
-iliyan
- * @author iliya
- */
-public class M05JUnitMaven {
-    
-
-  public static int add(int a, int b) {
-        return a + b;
-    }
-  
-}
-
  * @author alumne
  */
 public class M05JUnitMaven {
@@ -30,4 +15,3 @@ public class M05JUnitMaven {
         
     }
 }
- main
